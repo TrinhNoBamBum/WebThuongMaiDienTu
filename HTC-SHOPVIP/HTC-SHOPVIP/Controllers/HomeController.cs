4cd82b7cd4ec7346e07fda4008a620283378b7c7
TrinhNoBamBum <trinhcosi2xx@gmail.com>
2023-01-04 21:28:30 +0700

    Chuc nang Dang Nhap Dang Ki

--- HTC-SHOPVIP/HTC-SHOPVIP/Controllers/HomeController.cs
+++ HTC-SHOPVIP/HTC-SHOPVIP/Controllers/HomeController.cs
@@ -8,23 +8,91 @@ namespace HTC_SHOPVIP.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        // GET: Home
+        public ActionResult LoginView(string tb, string tb1, string tb2)
         {
+            ViewBag.tb1 = tb1;
+            ViewBag.tb2 = tb2;
+            ViewBag.tb = tb;
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login(string tendangnhap, string matkhau)
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.tb = "Tên tài khoản hoặc mật khẩu không đúng !";
+            using (var con = new Model1())
+            {
+                var tkmk = (from s in con.Khachhangs
+                            select s).ToList();
 
-            return View();
+                foreach (var item in tkmk)
+                {
+
+                    if (item.username.ToLower().Trim() == tendangnhap.ToLower().Trim() && item.matkhau.ToLower().Trim() == matkhau.ToLower().Trim())
+                    {
+
+                        var kh = (from s in con.Khachhangs
+                                  where s.username.ToLower().Trim() == tendangnhap.ToLower().Trim() && s.matkhau.ToLower().Trim() == matkhau.ToLower().Trim()
+                                  select s).FirstOrDefault();
+                        Session["khachhang"] = kh;
+                        return RedirectToAction("Index", "Home");
+
+                    }
+
+
+                }
+                return RedirectToAction("LoginView", "Home", new { tb = "Tên đăng nhập hoặc tài khoản sai rồi bạn ơi" });
+
+            }
+
         }
 
-        public ActionResult Contact()
+        public ActionResult DangKy(string hoten, string username, string pass, string repass, string sodt, string diachi, DateTime ngaysinh, string email)
         {
-            ViewBag.Message = "Your contact page.";
+            var khachh = new Khachhang();
+            var taikhoan = new TaiKhoan();
+            khachh.username = username;
+            khachh.ngaysinh = ngaysinh;
+            khachh.Hoten = hoten;
+            khachh.matkhau = pass;
+            khachh.diachi = diachi;
+            khachh.email = email;
+            khachh.sdt = sodt;
+            taikhoan.tk = username;
+            taikhoan.mk = pass;
+            taikhoan.isBuyer = true;
+            taikhoan.isSeller = false;
 
-            return View();
+            using (var con = new Model1())
+            {
+                int dem = (from s in con.Khachhangs
+                           where s.username.Trim().ToLower() == username.Trim().ToLower()
+                           select s).Count();
+                if (hoten == "" || username == "" || ngaysinh == null || pass == "" || repass == "" || diachi == "" || email == "" || sodt == "")
+                {
+                    return RedirectToAction("LoginView", "Home", new { tb1 = "Thông tin bắt buộc không được để trống" });
+                }
+                else if (pass != repass)
+                {
+                    return RedirectToAction("LoginView", "Home", new { tb1 = "Thông tin không chính xác" });
+                }
+                else if (dem > 0)
+                {
+                    return RedirectToAction("LoginView", "Home", new { tb1 = "Tên đăng nhập đã tồn tại" });
+                }
+                else
+                {
+                    con.Khachhangs.Add(khachh);
+                    con.SaveChanges();
+                    con.TaiKhoans.Add(taikhoan);
+                    con.SaveChanges();
+
+                    return RedirectToAction("LoginView", "Home", new { tb2 = "Đăng ký thành công" });
+                }
+
+            }
+
         }
+
     }
 }
